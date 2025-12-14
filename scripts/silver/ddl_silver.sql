9d6d023f51b5577633563197aedaca68a04d5049
Isha Ben Pulintara <ishapulintara2019@gmail.com>
2025-12-14 12:18:05 +0530

    Revise comments in ddl_silver.sql
    Updated comments and script purpose for clarity.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,5 +1,12 @@
 /*
-
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
 */
 
 if object_id('silver.crm_cust_info','u') is not null
